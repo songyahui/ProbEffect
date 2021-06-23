8401cb5724aa691e1ac7ec02e8c9b9466504c893
songyahui <songyahuiapp@hotmail.com>
2021-06-23 15:17:42 +0800

    added ast

--- ast.ml
+++ ast.ml
@@ -9,4 +9,19 @@ type expression =
     | Unit
     | Variable of string
     | Literal of literal
+
+type test = 
+    | Drop 
+    | Skip
+    | Test of field * int 
+    | Disj of test * test
+    | Conj of test * test
+    | Neg of test 
    
+type probNetKAT = 
+    | Filter of test
+    | Assign of field * int 
+    | Union of probNetKAT * probNetKAT 
+    | Sequence of probNetKAT * probNetKAT
+    | Choice of probNetKAT * probNetKAT * float 
+    | Iteration of probNetKAT
