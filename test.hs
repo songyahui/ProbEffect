7d8a643e70d6205f10922c4993b28270e3696cf6
songyahui <songyahuiapp@hotmail.com>
2021-07-30 22:22:47 +0800

    m

--- test.hs
+++ test.hs
@@ -5,35 +5,37 @@ increase f n = map (\ (a, b) -> (f * a, n : b))
 
 decreasingList :: Int -> [(Float, [Int])]
 decreasingList prevnum
-  | prevnum <= 1 = [(1.0, [1])]
+  | prevnum <= 1 = [(1.0, [])]
   | prevnum == 2
-  = let dice_1 = increase (1 / 6) 2 (decreasingList 1) in
-    let rest   = increase (5 / 6) 2 (decreasingList 1) in dice_1 ++ rest
+  = let dice_1 = increase (1 / 6) 1 (decreasingList 1) in
+    let rest   = [(5/6, [])] 
+    in dice_1 ++ rest
   | prevnum == 3
-  = let dice_1 = increase (1 / 6) 3 (decreasingList 1) in
-    let dice_2 = increase (1 / 6) 3 (decreasingList 2) in
-    let rest   = increase (4 / 6) 3 (decreasingList 1)
+  -- [] [1] [2] [2, 1]
+  = let dice_1 = increase (1 / 6) 1 (decreasingList 1) in
+    let dice_2 = increase (1 / 6) 2 (decreasingList 2) in
+    let rest   = [(4/6, [])] 
     in dice_1 ++ dice_2 ++ rest
   | prevnum == 4
-  = let dice_1 = increase (1 / 6) 4 (decreasingList 1) in
-    let dice_2 = increase (1 / 6) 4 (decreasingList 2) in
-    let dice_3 = increase (1 / 6) 4 (decreasingList 3) in
-    let   rest = increase (3 / 6) 4 (decreasingList 1)
+  = let dice_1 = increase (1 / 6) 1 (decreasingList 1) in
+    let dice_2 = increase (1 / 6) 2 (decreasingList 2) in
+    let dice_3 = increase (1 / 6) 3 (decreasingList 3) in
+    let   rest = [(3/6, [])] 
     in dice_1 ++ dice_2 ++ dice_3 ++ rest
   | prevnum == 5
-  = let dice_1 = increase (1 / 6) 5 (decreasingList 1) in
-    let dice_2 = increase (1 / 6) 5 (decreasingList 2) in
-    let dice_3 = increase (1 / 6) 5 (decreasingList 3) in
-    let dice_4 = increase (1 / 6) 5 (decreasingList 4) in
-    let   rest = increase (2 / 6) 5 (decreasingList 1)
+  = let dice_1 = increase (1 / 6) 1 (decreasingList 1) in
+    let dice_2 = increase (1 / 6) 2 (decreasingList 2) in
+    let dice_3 = increase (1 / 6) 3 (decreasingList 3) in
+    let dice_4 = increase (1 / 6) 4 (decreasingList 4) in
+    let   rest = [(2/6, [])] 
     in dice_1 ++ dice_2 ++ dice_3 ++ dice_4 ++ rest
   | otherwise  -- prevnum >= 6 
-  = let dice_1 = increase (1 / 6) 6 (decreasingList 1) in
-    let dice_2 = increase (1 / 6) 6 (decreasingList 2) in
-    let dice_3 = increase (1 / 6) 6 (decreasingList 3) in
-    let dice_4 = increase (1 / 6) 6 (decreasingList 4) in
-    let dice_5 = increase (1 / 6) 6 (decreasingList 5) in
-    let dice_6 = increase (2 / 6) 6 (decreasingList 1) 
+  = let dice_1 = increase (1 / 6) 1 (decreasingList 1) in
+    let dice_2 = increase (1 / 6) 2 (decreasingList 2) in
+    let dice_3 = increase (1 / 6) 3 (decreasingList 3) in
+    let dice_4 = increase (1 / 6) 4 (decreasingList 4) in
+    let dice_5 = increase (1 / 6) 5 (decreasingList 5) in
+    let dice_6 = [(1/6, [])] 
     in dice_1 ++ dice_2 ++ dice_3 ++ dice_4 ++ dice_5 ++ dice_6 
 
 compareList :: [Int] -> [Int] -> Bool
